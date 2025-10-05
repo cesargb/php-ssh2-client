c29a93cccbc31717f83eaab782b46240af57b389
Cesar <cesargb@gmail.com>
2025-10-05 21:47:42 +0200

    refactor

---
+++ src/SshClient.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cesargb\Ssh;
+
+final class SshClient
+{
+    private int $port = 22;
+
+    public function __construct(private string $host)
+    {
+    }
+
+    public function withPort(int $port): self
+    {
+        $this->port = $port;
+
+        return $this;
+    }
+
+    public function connect(): Session
+    {
+        return new Session($this)->connect($this->host, $this->port);
+    }
+}
