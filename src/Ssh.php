463d933b18a2c66169e2600f382b0c225659ccb6
Cesar <cesargb@gmail.com>
2025-10-05 18:21:28 +0200

    rename project

---
+++ src/Ssh.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Cesargb\Ssh;
+
+class Ssh
+{
+    //
+}
---
+++ tests/SshTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class SshTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
