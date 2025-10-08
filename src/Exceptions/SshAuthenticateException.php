fbc15413a45b45e21c2c0df8a82f9dba76e9a883
Cesar <cesargb@gmail.com>
2025-10-08 18:39:49 +0200

    fix

---
+++ src/Exceptions/SshAuthenticateException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cesargb\Ssh\Exceptions;
+
+class SshAuthenticateException extends SshException
+{
+    public function __construct(string $message = 'SSH Authentication Error')
+    {
+        parent::__construct($message);
+    }
+}
