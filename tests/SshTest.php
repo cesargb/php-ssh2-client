c3a6532ef854d321696d80f69373cca82f7460ae
Cesar <cesargb@gmail.com>
2025-10-08 18:25:39 +0200

    enable tests

--- tests/SshTest.php
+++ tests/SshTest.php
@@ -9,7 +9,7 @@ class SshTest extends TestCase
 {
     public function test_get_finger_print()
     {
-        $ssh = new SshClient('localhost');
+        $ssh = new SshClient('localhost')->withPort(2222);
 
         $connection = $ssh->connect();
 
@@ -21,20 +21,18 @@ class SshTest extends TestCase
         $connection->disconnect();
 
         $this->assertFalse($connection->isConnected());
-
     }
 
-    public function test_auth_with_key()
+    public function test_auth_with_auth()
     {
-        $ssh = new SshClient('localhost');
+        $ssh = new SshClient('localhost')->withPort(2222);
 
         $connection = $ssh->connect();
 
-        $connection->withAuthPublicKey('admin', __DIR__.'/id_rsa.pub', __DIR__.'/id_rsa');
+        $connection->withAuthPassword('root', 'secret123');
+
+        $this->assertTrue($connection->isAuthenticated());
 
         $connection->disconnect();
-
-        $this->assertFalse($connection->isConnected());
-
     }
 }
